06cb5424ac406138e930d4e82270bc7770313b2b
drearySyrup1 <gaizauskasaurimas@gmail.com>
2023-03-14 17:03:38 +0000

    update prices

--- items.js
+++ items.js
@@ -1,54 +1,54 @@
 export default [
   {
-    name: 'Cake (L)',
-    price: 11.5,
+    name: "Cake (L)",
+    price: 12,
     halfPrice: true,
   },
   {
-    name: 'Cake (M)',
-    price: 8.25,
+    name: "Cake (M)",
+    price: 8.99,
     halfPrice: true,
   },
   {
-    name: 'Cake (ra)',
-    price: 3.58,
+    name: "Cake (ra)",
+    price: 3.9,
     halfPrice: true,
   },
   {
-    name: 'Bread Full',
+    name: "Bread Full",
     price: null,
     populate: true,
     halfPrice: false,
   },
   {
-    name: 'Bread Half',
+    name: "Bread Half",
+    price: 1.09,
+    halfPrice: false,
+  },
+  {
+    name: "Senoliu",
+    price: 1.49,
+    halfPrice: false,
+  },
+  {
+    name: "Kaimiskas",
+    price: 1.2,
+    halfPrice: false,
+  },
+  {
+    name: "Arbatinis",
     price: 1,
-    halfPrice: false,
-  },
-  {
-    name: 'Senoliu',
-    price: 1.35,
-    halfPrice: false,
-  },
-  {
-    name: 'Kaimiskas',
-    price: 1.1,
-    halfPrice: false,
-  },
-  {
-    name: 'Arbatinis',
-    price: 0.9,
     halfPrice: false,
   },
 
   {
-    name: 'Small Pastry',
-    price: 0.88,
+    name: "Small Pastry",
+    price: 0.9,
     halfPrice: true,
   },
   {
-    name: 'Large Pastry',
-    price: 1.7,
+    name: "Large Pastry",
+    price: 1.85,
     halfPrice: true,
   },
 ];
